272e40a966d32ce6b464f35495f1026cb9bf5b46
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-11-18 21:01:52 -0200

    Structure app layout

--- app/src/main/java/com/jonathanrufino/babyonboard/MainActivity.java
+++ app/src/main/java/com/jonathanrufino/babyonboard/MainActivity.java
@@ -1,7 +1,11 @@
 package com.jonathanrufino.babyonboard;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_bodysigns:
+                        return true;
+                    case R.id.navigation_babycrib:
+                        return true;
+                    case R.id.navigation_connection:
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
