934cd2faabbef7feddfe021fbcaa6a5a725b9d45
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-11-24 21:18:57 -0200

    Create preferences helper to save rasp ip

---
+++ app/src/main/java/com/jonathanrufino/babyonboard/SharedPreferencesHelper.java
@@ -0,0 +1,21 @@
+package com.jonathanrufino.babyonboard;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPreferencesHelper {
+
+    public static void setSharedPreferenceString(Context context, String key, String value) {
+        SharedPreferences settings = context.getSharedPreferences(
+                context.getString(R.string.preferences_file), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public static String getSharedPreferenceString(Context context, String key, String defaultValue) {
+        SharedPreferences settings = context.getSharedPreferences(
+                context.getString(R.string.preferences_file), Context.MODE_PRIVATE);
+        return settings.getString(key, defaultValue);
+    }
+}
